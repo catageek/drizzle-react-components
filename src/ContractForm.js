f037fd5b5f2e6e72c8e8e60a641af7a0da6d2d68
Bertrand Masius <github@catageeks.tk>
2018-11-17 00:24:31 +0100

    Handle form errors and accept callbacks in this.props
    Also fix some dangerous access to this.state

--- src/ContractForm.js
+++ src/ContractForm.js
@@ -15,7 +15,7 @@ class ContractForm extends Component {
     const abi = this.props.drizzle.contracts[this.props.contract].abi;
 
     this.inputs = [];
-    var initialState = {};
+    var initialState = {inputs: {}};
 
     // Iterate over abi for correct function.
     for (var i = 0; i < abi.length; i++) {
@@ -23,13 +23,13 @@ class ContractForm extends Component {
             this.inputs = abi[i].inputs;
 
             for (var j = 0; j < this.inputs.length; j++) {
-                initialState[this.inputs[j].name] = { value: '', error: ''};
+                initialState.inputs[this.inputs[j].name] = { value: '', error: ''};
             }
 
             break;
         }
     }
-
+    initialState.error = '';
     this.state = initialState;
   }
 
@@ -42,49 +42,76 @@ class ContractForm extends Component {
           var inputLabel = this.props.labels ? this.props.labels[index] : input.name
           return (
             <React.Fragment key={input.name}>
-              <div><input type={inputType} name={input.name} value={this.state[input.name].value} placeholder={inputLabel} onChange={this.handleInputChange} /></div>
-              <Error text={this.state[input.name].error}/>
+              <div><input type={inputType} name={input.name} value={this.state.inputs[input.name].value} placeholder={inputLabel} onChange={this.handleInputChange} /></div>
+              <Error text={this.state.inputs[input.name].error} />
             </React.Fragment>
           )
         })}
+        <Error text={this.state.error} />
         <button key='submit' className='pure-button' type='button' onClick={this.handleSubmit}>Submit</button>
       </form>
     );
   }
 
   handleSubmit() {
-    // If an input is of type bytes32 then convert the entered text to hex, if it isn't already valid hex, using web3.
-    const values = this.inputs.map((input, i) => {
-      if (input.type === 'bytes32' && !this.props.drizzle.web3.utils.isHex(this.state[input.name].value)) { 
-        return this.props.drizzle.web3.utils.toHex(this.state[input.name].value);
-      } else {
-        return this.state[input.name].value;
+    this.setState((prevState) => {
+      // check if no error is left
+      let error = null;
+      for (let input in prevState.inputs) {
+        if (prevState.inputs[input].error !== '') {
+          error = "There are some errors in the formulary. Please fix them before retrying";
+          break;
+        }
       }
+      if (error) {
+        return ({error: error});
+      }
+      // no error
+      if (prevState.error !== '') {
+        return({error: ''});
+      }
+      // call callback function
+      let state = this.props.cb ? this.props.cb(prevState.inputs) : prevState.inputs;
+
+      // If an input is of type bytes32 then convert the entered text to hex, if it isn't already valid hex, using web3.
+      const values = this.inputs.map((input, i) => {
+        if (input.type === 'bytes32' && !this.props.drizzle.web3.utils.isHex(state[input.name].value)) { 
+          return this.props.drizzle.web3.utils.toHex(state[input.name].value);
+        } else {
+          return state[input.name].value;
+        }
+      });
+
+      if (this.props.sendArgs) {
+        return this.props.drizzle.contracts[this.props.contract].methods[this.props.method].cacheSend(...values, this.props.sendArgs);
+      }
+
+      this.props.drizzle.contracts[this.props.contract].methods[this.props.method].cacheSend(...values);
+
+      return prevState;
     });
 
-    if (this.props.sendArgs) {
-      return this.props.drizzle.contracts[this.props.contract].methods[this.props.method].cacheSend(...values, this.props.sendArgs);
-    }
-
-    this.props.drizzle.contracts[this.props.contract].methods[this.props.method].cacheSend(...values);
   }
 
   handleInputChange(event) {
-    const state = this.inputs.reduce((accumulator, input) => {
-      let error = '';
-      if (input.name === event.target.name) {
-        if (input.type === 'address') {
-          const missing = 42 - event.target.value.length;
-          error = missing ? 'number of chars missing: ' + missing:'';
-          if (event.target.value.length === 42 && !this.props.drizzle.web3.utils.checkAddressChecksum(event.target.value)) {
-            error = "Please enter an address with valid checksum";
+    event.persist();
+    this.setState((prevState) => {
+      return(this.inputs.reduce((accumulator, input) => {
+        let error = '';
+        if (input.name === event.target.name) {
+          if (input.type === 'address') {
+            const missing = 42 - event.target.value.length;
+            error = missing ? 'number of chars missing: ' + missing:'';
+            if (event.target.value.length === 42 && !this.props.drizzle.web3.utils.checkAddressChecksum(event.target.value)) {
+              error = "Please enter an address with valid checksum";
+            }
           }
+          return Object.assign(accumulator, { inputs: { ...prevState.inputs, [event.target.name]: { value: event.target.value, error: error }}});
         }
-        return Object.assign(accumulator, { [event.target.name]: { value: event.target.value, error: error }});
-      }
-      return accumulator;
-    }, {});
-    this.setState(state);
+        return accumulator;
+      }, prevState));
+    }
+    );
   }
 
   translateType(type) {
@@ -103,9 +130,9 @@ class ContractForm extends Component {
 
 function Error(props) {
   if (props.text !== '') {
-    return (<div className='input-error' style={{display: 'block', 'min-height': 23}}>{props.text}</div>);
+    return (<div className='input-error' style={{display: 'block', 'minHeight': 23}}>{props.text}</div>);
   }
-  return (<div style={{display: 'block', 'min-height': 23}}></div>);
+  return (<div style={{display: 'block', 'minHeight': 23}}></div>);
 }
 
 export default ContractForm;
